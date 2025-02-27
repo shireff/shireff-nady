907704b7e56fa5418dc4fef080df73d8120dfc5e
shireff <shireffn369@gmail.com>
2025-02-27 02:11:09 +0200

    Change for SEO

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone", 
+  generateMetadata: async () => ({
+    title: "Shireff Nady | Front-End Developer",
+    description: "Shireff Nady, a front-end developer skilled in HTML, CSS, JavaScript, React, and more.",
+  }),
 };
 
 export default nextConfig;
