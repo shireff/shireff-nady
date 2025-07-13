5ff39ec1b9406bd22623ffb3cdcbeab8430d2445
Shireff nady <shireffn369@gmail.com>
2025-07-13 18:45:53 +0300

    app script to sitemap generation pipeline

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://shireff-nady.vercel.app",
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: "monthly",
+  priority: 0.7,
+  exclude: [],
+};
