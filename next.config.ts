5a9a6145c974fee34e4df14ad3c765ec3dc42ad2
Shireff Nady <shireffn369@gmail.com>
2025-12-20 14:52:04 +0200

    feat: Bump Next.js and React versions, configure JSX runtime, and add global styling.

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
 
   output: "standalone",
-  generateMetadata: async () => ({
-    title: "Shireff Nady | Front-End Developer",
-    description:
-      "Shireff Nady, a front-end developer skilled in HTML, CSS, JavaScript, React, and more.",
-  }),
+
 
   images: {
     remotePatterns: [
